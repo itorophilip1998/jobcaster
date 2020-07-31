9f5a8617af47d9849131096309c65a61d87c2d25
itorophilip1998 <itkeyzzpianny@gmail.com>
2020-07-31 08:52:30 +0100

    make it better

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,6 +17,7 @@ class AuthController extends Controller
 {
 
     public function register(Request $request){
+        dd($request->all());
         // $validator = Validator::make($request->all(), [
         //     'name' => 'required|string|max:255',
         //     'email' => 'required|string|email|max:255|unique:users',
